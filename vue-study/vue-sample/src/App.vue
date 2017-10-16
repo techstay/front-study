06cbfa124b6580861f6429d1ed8df31af1a93be9
yitian <lovery521@gmail.com>
2017-10-17 02:42:14 +0800

    创建了Vue示例项目

---
+++ vue-study/vue-sample/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+
+    <div class="container">
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Vue示例项目</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
+                aria-controls="#navbar"
+                aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbar">
+          <ul class="navbar-nav mr-auto">
+            <router-link to="/" class="nav-item" tag="li">
+              <a class="nav-link">主页<span class="sr-only">(current)</span></a>
+            </router-link>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <br>
+    <div class="container">
+      <router-view/>
+
+      <hr>
+      <footer>
+        <p>&copy;2017 这是页脚</p>
+      </footer>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+</style>
---
+++ vue-study/vue-sample/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="jumbotron">
+    <h1>Vue示例项目</h1>
+    <p>
+      一行简单的文字，没有什么特殊含义。
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
